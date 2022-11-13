40ebfc069f77eeaa9674bcb1fffff95000a13923
sanam-umer <81857355+sanam-umer@users.noreply.github.com>
2022-11-13 17:50:46 +0530

    Update Dice.sol

--- contracts/Dice.sol
+++ contracts/Dice.sol
@@ -1,33 +1,54 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.6;
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.8.11;
+import './TRC20.sol';
 
-
-import './Token.sol';
-
-contract Dice is Token{
-    struct Game{
-      uint256 bet;
-      bool valid; //false when no bet is set 
+contract Game is TRC20{
+   
+   //1 TRX = 1000000 sun
+    struct Bet{
+        uint8 currentBet;  
+        bool isBetSet;
+        uint8 destiny;
     }
-    uint256 public repeats;
-    mapping(address => Game)public Games;// points details of game played by address
-
-    event gameSet(address player,uint256 bet,bool valid);
-    event newGame(uint256 repeats);
-
-   function PlayDice()public payable{
-    require(msg.value>0,'Place bet');
-    bet(msg.sender,msg.value);
-
-    Game memory game = Game(msg.value,true);
-    Games[player] = game;
-    emit newGame(repeats++);
-    emit gameSet(msg.sender,
-      msg.value,
-      true
-     );
-    }
-
+    mapping (address => Bet)private bets;
+    uint8 private randomFactor;
+ 
+    event newBetSet(address bidder,uint8 currentBet);
+    event GameResult(address bidder, uint8 currentBet, uint8 destiny);
     
-
+    // function transfer()external {
+    //      token = TRC20(TNq5PbSssK5XfmSYU4Aox4XkgTdpDoEDiY);
+    //     token.transfer(msg.sender,100);
+    // }
+    function getNewBet()public payable returns(uint8){
+        require(msg.value>0,'Bid higher amount');
+        require(bets[msg.sender].isBetSet == false);
+        bets[msg.sender].isBetSet == true ;
+        bets[msg.sender].currentBet = random();
+        randomFactor += bets[msg.sender].currentBet;
+        emit newBetSet((msg.sender),bets[msg.sender].currentBet);
+        return bets[msg.sender].currentBet;
+            }
+    
+    function roll()public returns(address,uint8,uint8){
+        require(bets[msg.sender].isBetSet == true,'place the bet');
+        bets[msg.sender].destiny = random();
+        randomFactor += bets[msg.sender].destiny;
+        bets[msg.sender].isBetSet = false;
+        if(bets[msg.sender].destiny == bets[msg.sender].currentBet){
+            transfer(msg.sender,1000000);
+            emit GameResult(msg.sender, bets[msg.sender].currentBet, bets[msg.sender].destiny);
+        }else{
+            emit GameResult(msg.sender,bets[msg.sender].currentBet, bets[msg.sender].destiny);
+        }return(msg.sender,bets[msg.sender].currentBet, bets[msg.sender].destiny);
+    }
+    function isBetSet() public view returns(bool){
+        return bets[msg.sender].isBetSet;
+    }
+    function random() private view returns (uint8) {
+        uint256 blockValue = uint256(blockhash(block.number-1 + block.timestamp));
+        blockValue = blockValue + uint256(randomFactor);
+        return uint8(blockValue % 5) + 1;
+   }
 }
+ 
